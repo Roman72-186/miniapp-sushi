17ef577f82b3661b5888c396bde9b50dbc03d7a3
Roman Mahmetov <mahmetov1988@bk.ru>
2025-09-19 23:34:52 +0500

    Обновление фото

--- src/data.js
+++ src/data.js
@@ -4,273 +4,273 @@ export const products = [
     "name": "Авокадо маки **",
     "description": "",
     "price": 0,
-    "image": "/public/img/avokado_maki.PNG"
+    "image": "/img/avokado_maki.PNG"
   },
   {
     "id": "9f6653e0-a26d-49a6-acf9-59c7462d07a4",
     "name": "Аге гурме **",
     "description": "",
     "price": 0,
-    "image": "/public/img/age_gurme.PNG"
+    "image": "/img/age_gurme.PNG"
   },
   {
     "id": "9f6653e0-a271-49d9-aaa2-a74a63eefbaa",
     "name": "Аляска кунсей **",
     "description": "",
     "price": 0,
-    "image": "/public/img/alaska_kunsei.PNG"
+    "image": "/img/alaska_kunsei.PNG"
   },
   {
     "id": "9f6653e0-a274-42ad-90cf-b422c509279b",
     "name": "Благородный сёгун **",
     "description": "",
     "price": 0,
-    "image": "/public/img/seegun.PNG"
+    "image": "/img/seegun.PNG"
   },
   {
     "id": "9fa8b803-1917-4d5f-802f-c19b912d4ddb",
     "name": "Бонито мидии **",
     "description": "",
     "price": 0,
-    "image": "/public/img/bonito_midii.PNG"
+    "image": "/img/bonito_midii.PNG"
   },
   {
     "id": "9fa8b803-1925-4487-9ba7-663f23f80d7e",
     "name": "Бонито тунец **",
     "description": "",
     "price": 0,
-    "image": "/public/img/bonito_tunec.PNG"
+    "image": "/img/bonito_tunec.PNG"
   },
   {
     "id": "9fa8b803-1928-430a-8dfa-fa839d683659",
     "name": "Боул с креветками **",
     "description": "",
     "price": 0,
-    "image": "/public/img/boul_s_krevetkami.PNG"
+    "image": "/img/boul_s_krevetkami.PNG"
   },
   {
     "id": "9f6653e0-a2bb-412c-af55-61ac30b7850f",
     "name": "Боул с лососем **",
     "description": "",
     "price": 0,
-    "image": "/public/img/boul_s_lososem.PNG"
+    "image": "/img/boul_s_lososem.PNG"
   },
   {
     "id": "9f6653e0-a2dc-4bb4-ae97-2840757855f9",
     "name": "Боул с тунцом **",
     "description": "",
     "price": 0,
-    "image": "/public/img/boul_s_tuncom.PNG"
+    "image": "/img/boul_s_tuncom.PNG"
   },
   {
     "id": "9f6653e0-a279-4163-9413-20ddcbdece6f",
     "name": "Бруклин **",
     "description": "",
     "price": 0,
-    "image": "/public/img/bruklin.PNG"
+    "image": "/img/bruklin.PNG"
   },
   {
     "id": "9f6653e0-a27c-44e1-bca7-85c2a93ab3d5",
     "name": "Гламур **",
     "description": "",
     "price": 0,
-    "image": "/public/img/glamur.PNG"
+    "image": "/img/glamur.PNG"
   },
   {
     "id": "9f6653e0-a27e-46c5-849d-b6960a486854",
     "name": "Горячий дон жуан **",
     "description": "",
     "price": 0,
-    "image": "/public/img/don_juan.PNG"
+    "image": "/img/don_juan.PNG"
   },
   {
     "id": "9f6653e0-a281-43dd-a51c-eed958b464ee",
     "name": "Гуччи **",
     "description": "",
     "price": 0,
-    "image": "/public/img/gujji.PNG"
+    "image": "/img/gujji.PNG"
   },
   {
     "id": "9f6653e0-a283-4478-95fa-42fb65f3818e",
     "name": "Детройт **",
     "description": "",
     "price": 0,
-    "image": "/public/img/detroit.PNG"
+    "image": "/img/detroit.PNG"
   },
   {
     "id": "9fa8b803-192a-46b8-bc5a-161213797d4f",
     "name": "Запеченный лайт **",
     "description": "",
     "price": 0,
-    "image": "/public/img/zapecheni_lite.PNG"
+    "image": "/img/zapecheni_lite.PNG"
   },
   {
     "id": "9f6653e0-a276-48ae-8285-f605f86aefad",
     "name": "Запеченный лосось файр **",
     "description": "",
     "price": 0,
-    "image": "/public/img/losos_fair.PNG"
+    "image": "/img/losos_fair.PNG"
   },
   {
     "id": "9f6653e0-a288-4c36-8494-f1c052072786",
     "name": "Запеченный мидуэй **",
     "description": "",
     "price": 0,
-    "image": "/public/img/Miduei.PNG"
+    "image": "/img/Miduei.PNG"
   },
   {
     "id": "9f6653e0-a28a-4215-b9e1-775d96403984",
     "name": "Запеченный мистер Грин **",
     "description": "",
     "price": 0,
-    "image": "/public/img/miduei(1).PNG"
+    "image": "/img/miduei(1).PNG"
   },
   {
     "id": "9f6653e0-a28d-41c4-95af-26c9e7aa7ad9",
     "name": "Запеченный ролл с беконом **",
     "description": "",
     "price": 0,
-    "image": "/public/img/roll_s_bekonom.PNG"
+    "image": "/img/roll_s_bekonom.PNG"
   },
   {
     "id": "9fa8b803-192d-45a8-9067-6f429772d721",
     "name": "Запеченный тори ролл **",
     "description": "",
     "price": 0,
-    "image": "/public/img/tori_roll.PNG"
+    "image": "/img/tori_roll.PNG"
   },
   {
     "id": "9fa8b803-192f-4b2a-998b-23729a14221d",
     "name": "Игуана **",
     "description": "",
     "price": 0,
-    "image": "/public/img/iguana.PNG"
+    "image": "/img/iguana.PNG"
   },
   {
     "id": "9f6653e0-a28f-4c1e-b4ac-e89b58a15174",
     "name": "Калифорния **",
     "description": "",
     "price": 0,
-    "image": "/public/img/kalifornia.PNG"
+    "image": "/img/kalifornia.PNG"
   },
   {
     "id": "9f6653e0-a292-4040-b788-7a212640e041",
     "name": "Кани гриль **",
     "description": "",
     "price": 0,
-    "image": "/public/img/kani_gril.PNG"
+    "image": "/img/kani_gril.PNG"
   },
   {
     "id": "9f6653e0-a294-4f24-a569-acfaaef8c4fc",
     "name": "Каппа маки **",
     "description": "",
     "price": 0,
-    "image": "/public/img/kappa_maki.PNG"
+    "image": "/img/kappa_maki.PNG"
   },
   {
     "id": "9fa8b803-1932-4524-b358-01aca326a85a",
     "name": "Киото чикен **",
     "description": "",
     "price": 0,
-    "image": "/public/img/kioto_chiken.PNG"
+    "image": "/img/kioto_chiken.PNG"
   },
   {
     "id": "9f6653e0-a297-4dbd-967e-a40f4cbae028",
     "name": "Краб дуэт гриль **",
     "description": "",
     "price": 0,
-    "image": "/public/img/krab_duet.PNG"
+    "image": "/img/krab_duet.PNG"
   },
   {
     "id": "9f6653e0-a29b-45d0-9748-243199608dcd",
     "name": "Маленький принц **",
     "description": "",
     "price": 0,
-    "image": "/public/img/mal_princ.PNG"
+    "image": "/img/mal_princ.PNG"
   },
   {
     "id": "9f6653e0-a29e-473f-94ee-f7aeb0ff9e25",
     "name": "Манхеттен **",
     "description": "",
     "price": 0,
-    "image": "/public/img/manheten.PNG"
+    "image": "/img/manheten.PNG"
   },
   {
     "id": "9f6653e0-a299-4b4c-8876-0e53f4a21263",
     "name": "Мидии маки гриль **",
     "description": "",
     "price": 0,
-    "image": "/public/img/midii_maki_gril.PNG"
+    "image": "/img/midii_maki_gril.PNG"
   },
   {
     "id": "9fa8b803-1934-413a-b034-1e3fd30c6190",
     "name": "Мидии терияки **",
     "description": "",
     "price": 0,
-    "image": "/public/img/midii_teriaki.PNG"
+    "image": "/img/midii_teriaki.PNG"
   },
   {
     "id": "9f6653e0-a2a0-4027-b09f-e48f1b4ea89e",
     "name": "Нежный поцелуй **",
     "description": "",
     "price": 0,
-    "image": "/public/img/nejni_kiss.PNG"
+    "image": "/img/nejni_kiss.PNG"
   },
   {
     "id": "9f6653e0-a2a3-44d7-8bee-7a20eb62ff6e",
     "name": "Ниагара **",
     "description": "",
     "price": 0,
-    "image": "/public/img/niagara.PNG"
+    "image": "/img/niagara.PNG"
   },
   {
     "id": "9f6653e0-a2a5-4482-b883-1eb1dd4ce5c3",
     "name": "Пикантный лосось **",
     "description": "",
     "price": 0,
-    "image": "/public/img/picantnii_losos.jpg"
+    "image": "/img/picantnii_losos.jpg"
   },
   {
     "id": "9f6653e0-a2b1-4a44-bba6-3c096aee09cf",
     "name": "Поке с жареным лососем **",
     "description": "",
     "price": 0,
-    "image": "/public/img/poke_s_lososem.jpg"
+    "image": "/img/poke_s_lososem.jpg"
   },
   {
     "id": "9f6653e0-a2b6-4ad5-8d55-d402cfffd939",
     "name": "Поке с креветками **",
     "description": "",
     "price": 0,
-    "image": "/public/img/poke_s_krevetk.jpg"
+    "image": "/img/poke_s_krevetk.jpg"
   },
   {
     "id": "9f6653e0-a2a8-4120-a6b3-35607501f344",
     "name": "Розовый **",
     "description": "",
     "price": 0,
-    "image": "/public/img/pink.PNG"
+    "image": "/img/pink.PNG"
   },
   {
     "id": "9f6653e0-a2aa-4d7c-99ba-1cb659df6332",
     "name": "Самурай **",
     "description": "",
     "price": 0,
-    "image": "/public/img/samurai.PNG"
+    "image": "/img/samurai.PNG"
   },
   {
     "id": "9fa8b803-1939-4ab7-b23f-9481bb1413fa",
     "name": "Сказочный тунец **",
     "description": "",
     "price": 0,
-    "image": "/public/img/skaz_tunec.jpg"
+    "image": "/img/skaz_tunec.jpg"
   },
   {
     "id": "9f6653e0-a2ac-40c5-a0f2-a9f6a01ca393",
     "name": "Спайси эби маки **",
     "description": "",
     "price": 0,
-    "image": "/public/img/spaisi_ebi_maki.jpg"
+    "image": "/img/spaisi_ebi_maki.jpg"
   },
   {
     "id": "9f6653e0-a2af-47df-b8eb-19a9dc4d934b",
